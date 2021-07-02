d9a959ef18b53c20575bdaa3c72512805cb053ea
VISHESHK27 <61914430+visheshk27@users.noreply.github.com>
2021-07-02 17:29:57 +0530

    Create INIT.ino

---
+++ INIT.ino
@@ -0,0 +1,20 @@
+#include "DigiKeyboard.h"
+void setup() {
+  //we need to open MLH challenges and schedule page daily don't worry use any HID Microcontroller use the script convert it you want just plug it in and you need not to worry about opening it
+}
+void loop() {
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("https://init.mlh.io/challenges");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(5000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.print("https://init.mlh.io/schedule");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_F11);
+  for(;;){ /*empty*/ }
+}
